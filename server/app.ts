a24843d53daa760b8e4cd87da00db52dde3b7142
aashish_k <aashishkathat00@gmail.com>
2024-05-03 04:07:31 +0530

    serve static files

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,14 @@
 import { Hono } from 'hono';
+import { serveStatic } from 'hono/bun';
 import { logger } from 'hono/logger';
 import { expenseRouter } from './routes/expenses';
 
 const app = new Hono();
 
 app.use('*', logger());
-app.get('/test', (c) => c.json({ msg: 'doing it ' }));
 app.route('/api/expense', expenseRouter);
+
+app.get('*', serveStatic({ root: '../client/dist' }));
+app.get('*', serveStatic({ path: '../client/dist/index.html' }));
 
 export default app;

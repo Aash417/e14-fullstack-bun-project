9bf111d7b44753a229aaa557dedf8248e2033bbd
aashish_k <aashishkathat00@gmail.com>
2024-05-03 01:12:04 +0530

    setting up frontend

--- server/app.ts
+++ server/app.ts
@@ -2,12 +2,9 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { expenseRouter } from './routes/expenses';
 
-
-
-
 const app = new Hono();
 
-app.use('*', logger())
+app.use('*', logger());
 app.get('/test', (c) => c.json({ msg: 'doing it ' }));
 app.route('/api/expense', expenseRouter);
 

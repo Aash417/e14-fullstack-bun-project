fcd89465e744ea21bc2a6fa1ec7ce315cbe511c7
aashish_k <aashishkathat00@gmail.com>
2024-05-04 05:56:43 +0530

    added kinde auth

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,14 @@
 import { Hono } from 'hono';
 import { serveStatic } from 'hono/bun';
 import { logger } from 'hono/logger';
+import { authRoute } from './routes/auth';
 import { expenseRouter } from './routes/expenses';
 
 const app = new Hono();
 
 app.use('*', logger());
 
-const apiRoute = app.basePath('/api').route('/expense', expenseRouter);
+const apiRoute = app.basePath('/api').route('/expense', expenseRouter).route('/', authRoute);
 
 app.get('*', serveStatic({ root: '../client/dist' }));
 app.get('*', serveStatic({ path: '../client/dist/index.html' }));
